15726fad12ea853b763f36f39b160f71b0df1baf
Yasin Sa'adu <137645846+yasinsaadu@users.noreply.github.com>
2025-08-28 21:31:13 +0100

    Add files via upload

---
+++ text based game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void namerequest(){
+  cout<<"Welcome, adventurer. What would you like to be called? ";
+}
+
+void questreport(){
+  cout<<"The Demon King has just raised his army of the undead and he reigning terror upon the Continent."<<endl;
+  cout<<"The Continent is in dire need of a great band of heroes to stop this army of destruction and terror."<<endl;
+  cout<<"Before we continue, I must warn you, death awaits all who choose to fight, but we believe that there will be a light of hope at the end of the tunnel that would deem us victorius= in our pursuit for glory and freedom."<<endl;
+}
+
+int main(){
+  string name;
+  namerequest();
+  getline(cin, name);
+  cout<<"\nAlright! Adventurer " << name << ", you will be briefed about the quest shortly."<<endl;
+  questreport();
+
+  return 0;
+}
